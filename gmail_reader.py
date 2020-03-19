b1f5a379e67775bccf10f57881938b48744f21b5
Nick <sshedl22@gmail.com>
2020-03-19 12:08:16 +0300

    Added opportunity to show attachments count. Still need to do some clean up.

--- gmail_reader.py
+++ gmail_reader.py
@@ -10,7 +10,6 @@ from googleapiclient.errors import HttpError
 import html2text
 import pprint
 from email.header import decode_header
-from outside import getmailheader
 from numpy.core import unicode
 
 # source 1 https://github.com/abhishekchhibber/Gmail-Api-through-Python/blob/master/gmail_read.py
@@ -25,14 +24,15 @@ class GmailAgent:
         # The file token.pickle stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
-        self.message = None
-        self.message_info = dict()
+        self._message = None
+        self._message_info = dict()
+        # TODO: print Attachment's names
         if is_token_available:
             with open('token.pickle', 'rb') as token:
                 creds = pickle.load(token)
         # If there are no (valid) credentials available, let the user log in.
         self._login_and_save_token(creds)
-        self.service = build('gmail', 'v1', credentials=creds)
+        self._service = build('gmail', 'v1', credentials=creds)
 
     # def _find_massage_in_mailbox(self, user_id='me', label_id="UNSEEN"):
     #     """List all Messages of the user's mailbox with label_ids applied.
@@ -69,46 +69,24 @@ class GmailAgent:
     def get_info_from_message(self):
         #         self.message = None
         #         self.message_info = dict()
-        for message_id in ListMessagesWithLabels(self.service, 'me', ["INBOX"]):
-            self.message = GetMimeMessage(self.service, 'me', message_id["id"])
+        for message_id in ListMessagesWithLabels(self._service, 'me', ["INBOX"]):
+            self._message_info["Attach_Num"] = 0
+            self._message = GetMimeMessage(self._service, 'me', message_id["id"])
             self._parse_header()
-            self._parse_body()
+            self._parse_body(self._message)
             self._get_message_snippet()
-            pprint.pprint(self.message_info)
+            pprint.pprint(self._message_info)
 
-    def _parse_body(self):
-        # Get the messages
-        # charset = msg.get_param('charset', 'utf-8').lower()
-        # update charset aliases
-        # charset = email.charset.ALIASES.get(charset, charset)
-        # msg.set_param('charset', charset)
-        body = self.message_info["Body"] = list()
-        msg = self.message
+    def _parse_body(self, msg):
+        self._message_info["Body"] = list()
         if msg.is_multipart():
-            main_content = None
-            # multi-part emails often have both
-            # a text/plain and a text/html part.
-            # Use the first `text/plain` part if there is one,
-            # otherwise take the first `text/*` part.
-            for part in msg.get_payload():
-                is_txt = part.get_content_type() == 'text/plain'
-                if not main_content or is_txt:
-                    main_content = extract_body(part)
-                if is_txt:
-                    break
-            if main_content:
-                body.extend(main_content)
+            self._parse_body_part(msg)
         elif msg.get_content_type().startswith("text/"):
-            if self.message.get_content_type() == "text/plain":
-                body.append(self._handle_plain_text())
-            elif self.message.get_content_type() == "text/html":
-                body.append(self._handle_html_text())
-            else:
-                raise TypeError("Unsupportable type")
+            self._parse_text_content(msg)
+        return self._message_info["Body"]  # ugly
 
-    def _handle_plain_text(self):
-        body = list()
-        msg = self.message
+    def _handle_plain_text(self, msg):
+        body = self._message_info["Body"]
         # msg.get_params() - return 2 tuples. Second element in second tuple stores letter encoding.
         encoding = msg.get_params()[1][1]
         if encoding == 'flowed':
@@ -116,23 +94,20 @@ class GmailAgent:
         else:
             base64_info = msg.get_payload(decode=True)
             clean_two = base64_info.decode(encoding)
-        body.append(clean_two)
-        return body
+        body.append([clean_two])
         # TODO: add exception handler
 
-    def _handle_html_text(self):
-        body = list()
-        msg = self.message
+    def _handle_html_text(self, msg):
+        body = self._message_info["Body"]
         base64_info = msg.get_payload()
         text = html2text.html2text(base64_info)
-        body.append(text)
-        return body
+        body.append([text])
 
     def _parse_header(self):
-        self.message_info["Subject"] = self._parse_parameter_from_header(self.message.get('Subject',' '))
-        self.message_info["From"] = self._parse_parameter_from_header(self.message.get('From'))
-        self.message_info["To"] = self._parse_parameter_from_header(self.message.get('To'))
-        self.message_info["Date"] = self._parse_date_from_header()
+        self._message_info["Subject"] = self._parse_parameter_from_header(self._message.get('Subject', ' '))
+        self._message_info["From"] = self._parse_parameter_from_header(self._message.get('From'))
+        self._message_info["To"] = self._parse_parameter_from_header(self._message.get('To'))
+        self._message_info["Date"] = self._parse_date_from_header()
 
     def _parse_parameter_from_header(self, parameter):
         default_encoding = "utf-8"
@@ -154,11 +129,11 @@ class GmailAgent:
             return u"".join(headers)
 
     def _parse_date_from_header(self):
-        date = parser.parse(self.message.get('Received').split(";")[1].strip()).date().strftime("%d/%m/%Y")
+        date = parser.parse(self._message.get('Received').split(";")[1].strip()).date().strftime("%d/%m/%Y")
         return date
 
     def _get_message_snippet(self):
-        self.message_info['Snippet'] = self.message['snippet']
+        self._message_info['Snippet'] = self._message['snippet']
 
     def _login_and_save_token(self, creds):
         # !!!important creds will be modified
@@ -173,6 +148,32 @@ class GmailAgent:
             with open('token.pickle', 'wb') as token:
                 pickle.dump(creds, token)
         return creds
+
+    def _parse_body_part(self, msg):
+        # multi-part emails often have both
+        # a text/plain and a text/html part.
+        # Use the first `text/plain` part if there is one,
+        # otherwise take the first `text/*` part.
+        body = self._message_info["Body"]
+        main_content = None
+        for part in msg.get_payload():
+            is_txt = part.get_content_type() == 'text/plain'
+            if not main_content or is_txt:
+                main_content = self._parse_body(part)
+            if part.get('Content-Disposition'):
+                self._message_info["Attach_Num"] += 1
+            if is_txt:
+                break
+        if main_content:
+            body.extend(main_content)
+
+    def _parse_text_content(self, msg):
+        if msg.get_content_type() == "text/plain":
+            self._handle_plain_text(msg)
+        elif msg.get_content_type() == "text/html":
+            self._handle_html_text(msg)
+        else:
+            raise TypeError("Unsupportable type")
 
 
 def GetMimeMessage(service, user_id, msg_id):
